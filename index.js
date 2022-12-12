d542c482eaf4ec7544ae15ef8bbd0dfecad7e514
njiru-kamau <maureenkamau947@gmail.com>
2022-12-12 07:08:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+  alert('Button clicked!');
+});
+
 }
+
+
+const button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+  alert('Button clicked!');
+});
